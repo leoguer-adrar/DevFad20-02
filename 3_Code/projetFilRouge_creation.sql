b6159a08776c05251e4eca22b067c4f339e4a149
patl65 <layerle.p@gmail.com>
2020-12-04 00:02:55 +0100

    SQL
    Création et requettes

---
+++ 3_Code/projetFilRouge_creation.sql
@@ -0,0 +1,72 @@
+CREATE DATABASE ProjetFilRouge;
+USE ProjetFilRouge;
+CREATE TABLE Projet(
+    id_projet Int Auto_increment NOT NULL,
+    nom_projet Varchar (250) NOT NULL,
+    prestation_projet Varchar (100) NOT NULL,
+    maitre_ouvrage_projet Varchar (150) NOT NULL,
+    montant_operation_ht_projet Varchar (20) NOT NULL,
+    surface_projet Varchar (100) NOT NULL,
+    etat_projet Varchar (50) NOT NULL,
+    date_fin_projet Varchar (4) NOT NULL,
+    doc1_projet Varchar (50) NOT NULL,
+    doc2_projet Varchar (50),
+    doc3_projet Varchar (50),
+    doc4_projet Varchar (50),
+    doc5_projet Varchar (50),
+    CONSTRAINT Projet_PK PRIMARY KEY (id_projet)
+) ENGINE = InnoDB;
+CREATE TABLE Utilisateur(
+    id_utilisateur Int Auto_increment NOT NULL,
+    nom_utilisateur Varchar (100) NOT NULL,
+    prenom_utilisateur Varchar (100) NOT NULL,
+    mail_utilisateur Varchar (50) NOT NULL,
+    password_utilisateur Varchar (10) NOT NULL,
+    telephone_utilisateur Varchar (15) NOT NULL,
+    Adresse_ligne1_utilisateur Varchar (50) NOT NULL,
+    Adresse_ligne2_utilisateur Varchar (50) NOT NULL,
+    Code_postal_utilisateur Varchar (6) NOT NULL,
+    ville_utilisateur Varchar (50) NOT NULL,
+    pays_utilisateur Varchar (20) NOT NULL,
+    administrateur_utilisateur Bool NOT NULL,
+    CONSTRAINT Utilisateur_PK PRIMARY KEY (id_utilisateur)
+) ENGINE = InnoDB;
+CREATE TABLE Actualite(
+    id_actualite Int Auto_increment NOT NULL,
+    date_actualite Varchar (4) NOT NULL,
+    titre_actualite Varchar (500) NOT NULL,
+    article_actualite Varchar (100) NOT NULL,
+    photo1_actualite Varchar (50),
+    photo2_actualite Varchar (50),
+    photo3_actualite Varchar (50),
+    photo4_actualite Varchar (50),
+    photo5_actualite Varchar (50),
+    CONSTRAINT Actualite_PK PRIMARY KEY (id_actualite)
+) ENGINE = InnoDB;
+CREATE TABLE Demande_devis(
+    id_devis Int Auto_increment NOT NULL,
+    date_devis Date NOT NULL,
+    titre_devis Varchar (100) NOT NULL,
+    article_devis Varchar (500) NOT NULL,
+    photo1_projet Varchar (50),
+    photo2_projet Varchar (50),
+    CONSTRAINT Demande_devis_PK PRIMARY KEY (id_devis)
+) ENGINE = InnoDB;
+CREATE TABLE Commentaire_devis(
+    id_commentaire_devis Int Auto_increment NOT NULL,
+    commentaire_devis Varchar (50) NOT NULL,
+    id_devis Int,
+    CONSTRAINT Commentaire_devis_PK PRIMARY KEY (id_commentaire_devis),
+    CONSTRAINT Commentaire_devis_Demande_devis_FK FOREIGN KEY (id_devis) REFERENCES Demande_devis(id_devis)
+) ENGINE = InnoDB;
+CREATE TABLE relation0(
+    id_utilisateur Int NOT NULL,
+    id_actualite Int NOT NULL,
+    id_projet Int NOT NULL,
+    id_devis Int NOT NULL,
+    CONSTRAINT relation0_PK PRIMARY KEY (id_utilisateur, id_actualite, id_projet, id_devis),
+    CONSTRAINT relation0_Utilisateur_FK FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
+    CONSTRAINT relation0_Actualite0_FK FOREIGN KEY (id_actualite) REFERENCES Actualite(id_actualite),
+    CONSTRAINT relation0_Projet1_FK FOREIGN KEY (id_projet) REFERENCES Projet(id_projet),
+    CONSTRAINT relation0_Demande_devis2_FK FOREIGN KEY (id_devis) REFERENCES Demande_devis(id_devis)
+) ENGINE = InnoDB;
---
+++ 3_Code/projetFilRouge_requettes.sql
@@ -0,0 +1,122 @@
+/*3.	Requêtes CRUD :
+ Rédiger les requêtes CRUD suivantes :
+ -Insertion de données dans vos tables,
+ -Modification de données dans vos tables,
+ -Suppression de données dans vos tables,
+ -Sélection de données dans vos tables avec des jointures,
+ */
+
+
+INSERT INTO Projet(nom_projet, prestation_projet, maitre_ouvrage_projet, montant_operation_ht_projet, surface_projet, etat_projet, date_fin_projet, doc1_projet, doc2_projet, doc3_projet, doc4_projet, doc5_projet)
+ VALUES ("villa titi", "Esquisse à Permis de construire", "Pascale, 65000 Tarbes", "100 000 €", "150 m²", "Réalisé", "2018","doc 1 villa titi","","","","");
+INSERT INTO Projet(nom_projet, prestation_projet, maitre_ouvrage_projet, montant_operation_ht_projet, surface_projet, etat_projet, date_fin_projet, doc1_projet, doc2_projet, doc3_projet, doc4_projet, doc5_projet)
+ VALUES ("villa toto", "Mission de base", "Paul, 65000 Tarbes", "300 000 €", "150 m²", "Réalisé", "2018","doc 1 villa toto","doc 2 villa toto","doc 3 villa toto","doc 4 villa toto","doc 5 villa toto"),
+         ("villa tata", "Mission de base Esquisse à Réception chantier", "Privé", "180 000 €", "site : 6 400 m² bâtiments : 780 m²", "Réalisé", "2019","doc 1 villa tata","doc 2 villa tata","doc 3 villa tata","",""),
+         ("villa mimi", "Esquisse à Permis de construire", "Pierre, 65000 Tarbes", "60 000 €", "150 m²", "Réalisé", "2019","doc 1 villa mimi","","","",""),
+         ("villa momo", "Esquisse à Permis de construire", "Dépantement, 65000 Tarbes", "210 000 €", "300 m²", "Réalisé", "2019","doc 1 villa momo","","","",""),
+         ("villa mama", "Mission de base", "Pascale, 65000 Tarbes", "120 000 €", "150 m²", "Réalisé", "2019","doc 1 villa mama", "doc 2 villa mama","","","");
+
+
+
+INSERT INTO Utilisateur(nom_utilisateur, prenom_utilisateur, mail_utilisateur, password_utilisateur, telephone_utilisateur, Adresse_ligne1_utilisateur, Adresse_ligne2_utilisateur, Code_postal_utilisateur, ville_utilisateur, pays_utilisateur, administrateur_utilisateur)
+VALUES ("Moreau", "Anne", "am@gmail.com", "azerty1/", "06-07-08-09-10", "16 rue du Pic du Midi", "", "65000", "Tarbes", "France", true);
+INSERT INTO Utilisateur(nom_utilisateur, prenom_utilisateur, mail_utilisateur, password_utilisateur, telephone_utilisateur, Adresse_ligne1_utilisateur, Adresse_ligne2_utilisateur, Code_postal_utilisateur, ville_utilisateur, pays_utilisateur, administrateur_utilisateur)
+VALUES ("Layerle", "Patricia", "pl@gmail.com", "azerty1/", "06-07-08-09-10", "65 rue du Pic du Midi", "", "65310", "Horgues", "France", true),
+       ("Didou", "Elisa", "ed@gmail.com", "azerty1/", "06-07-08-09-10", "20 rue du Pic du Midi", "Villa Soleuil", "65310", "Laloubere", "France", false),
+       ("Dada", "Jérôme", "jd@gmail.com", "azerty1/", "06-07-08-09-10", "30 rue du Pic du Midi", "", "65310", "Odos", "France", false),
+       ("Sorbet", "Khang", "ks@gmail.com", "azerty1/", "06-07-08-09-10", "16 rue du Pic du Midi", "Appartement 51", "65360", "Momères", "France", false),
+       ("Dalier", "Alexis", "ad@gmail.com", "azerty1/", "06-07-08-09-10", "59 rue du Pic du Midi", "", "65360", "Saint-Martin", "France", false),
+       ("Alric", "Alex", "aa@gmail.com", "azerty1/", "06-07-08-09-10", "78 rue du Pic du Midi", "Maison Bueno", "65360", "Arcizac", "France", false),
+       ("Pipereau", "Romain", "rp@gmail.com", "azerty1/", "06-07-08-09-10", "16 rue du Pic du Midi", "", "65200", "Bagnères de Bigorre", "France", false),
+       ("Marcos", "Hugo", "hm@gmail.com", "azerty1/", "06-07-08-09-10", "40 rue du Pic du Midi", "Maison Nina", "65420", "Ibos", "France", false),
+       ("Humber", "Corine", "ch@gmail.com", "azerty1/", "06-07-08-09-10", "26 rue du Pic du Midi", "", "65700", "Juillan", "France", false),
+       ("Marty", "Rémi", "rm@gmail.com", "azerty1/", "06-07-08-09-10", "75 rue du Pic du Midi", "", "65200", "Pouzac", "France", false),
+       ("Dallas", "Florian", "fd@gmail.com", "azerty1/", "06-07-08-09-10", "60 rue du Pic du Midi", "", "65300", "Lourdes", "France", false);
+/*Mettre par défaut la valeur false (faux = 0 (true=vrai=1)) au champs Administrateur*/
+ALTER TABLE `utilisateur`
+	CHANGE COLUMN `administrateur_utilisateur` `administrateur_utilisateur` TINYINT(1) NOT NULL DEFAULT '0' AFTER `pays_utilisateur`;
+/*Pour changer les A en a de adresse*/
+ALTER TABLE `utilisateur`
+	ALTER `Adresse_ligne1_utilisateur` DROP DEFAULT,
+	ALTER `Adresse_ligne2_utilisateur` DROP DEFAULT;
+ALTER TABLE `utilisateur`
+	CHANGE COLUMN `Adresse_ligne1_utilisateur` `adresse_ligne1_utilisateur` VARCHAR(50) NOT NULL AFTER `telephone_utilisateur`,
+	CHANGE COLUMN `Adresse_ligne2_utilisateur` `adresse_ligne2_utilisateur` VARCHAR(50) NOT NULL AFTER `adresse_ligne1_utilisateur`;
+/*entrer données*/
+A faire
+
+
+
+
+
+
+
+
+
+/*changer le type du champs date_actualite mis en varchar par erreur*/
+ALTER TABLE `actualite`
+	ALTER `date_actualite` DROP DEFAULT;
+ALTER TABLE `actualite`
+	CHANGE COLUMN `date_actualite` `date_actualite` DATE NOT NULL AFTER `id_actualite`;
+/*Saisie données - date au format américain pour quelle soit acceptée*/
+INSERT INTO Actualite(date_actualite, titre_actualite, article_actualite, photo1_actualite, photo2_actualite, photo3_actualite, photo4_actualite, photo5_actualite)
+VALUES ("2020/12/03", "EPAD Horgues", "Le chantier avance bien après 11 mois de travaux et 1 confinement", "Photo1", "Photo2", "", "", ""),
+       ("2020/11/27", "Mairie", "Le chantier est fini !!", "Photo mairie sud", "Photo mairie nord", "", "", ""),
+       ("2020/11/15", "Eglise", "Réahibilitation toujours en cours", "Eglise ouest", "", "", "", ""),
+       ("2020/11/04", "Maison Soleil", "Jaune soleil", "soleil 1", "Soleil 2", "", "", ""),
+       ("2020/10/21", "Villa des Montagnes", "Mais ù est la neige", "neige", "montagne", "", "", ""),
+       ("2020/10/09", "Maison 65", "Bon anniversaire Patriicia !!!", "Chocolatine", "Gâteau", "Cadeau 1", "Cadeau 2", "Cadeau 3");
+
+
+INSERT INTO Demande_devis(date_devis, titre_devis, article_devis, photo1_projet, photo2_projet)
+VALUES ("2020/12/03", "article du jour", "Hep !!! Bonjour", "Photo 1", "Photo 2"),
+        ("2020/12/03", "article 2", "Article !!! Bonjour", "", ""),
+        ("2020/12/03", "article 3", "Arttttttttttt !!! Bonjour", "Hello", "Hola"),
+        ("2020/12/03", "article 4", "Joyeux Noêl !!! ", "Père Noël", ""),
+        ("2020/12/03", "article 5", "Bonne année !!! ", "Houx", "Galette des rois"),
+        ("2020/12/03", "article 6", "Meilleurs voeux !!! 2021", "Bonheur", "Santé");
+
+
+INSERT INTO Commentaire_devis(commentaire_devis, id_devis)
+VALUES ("RE-bonjour", "1"),
+        ("Sapin de Noël !!","4"),
+        ("Formation développeuse validée !!!", "6"),
+        ("Félicitations !!!!!!!", "6");
+
+
+
+
+
+/*ajouter id_commentaire_devis à la table et saisir*/
+
+INSERT INTO relation0(id_utilisateur, id_actualite, id_projet, id_devis, id_commentaire_devis)
+VALUES("1", "7", "", "", ""),
+      ("1", "8", "", "", ""),
+      ("1", "9", "", "", ""),
+      ("1", "10", "", "", ""),
+      ("1", "11", "", "", ""),
+      ("1", "12", "","", ""),
+      ("1", "", "1", "", ""),
+      ("1", "", "2", "", ""),
+      ("1", "", "3","", ""),
+      ("1", "", "4", "", ""),
+      ("1", "", "5", "", ""),
+      ("1", "", "6", "", ""),
+      ("1", "", "", "1", ""),
+      ("1", "", "", "2", ""),
+      ("1", "", "", "3", ""),
+      ("1", "", "", "4", ""),
+      ("1", "", "", "5", ""),
+      ("1", "", "", "6", ""),
+      ("2", "", "", "", "1"),
+      ("2", "", "", "", "2"),
+      ("2", "", "", "", "3"),
+      ("2", "", "", "", "4"),
+
+
+
+
+
+
+
+
