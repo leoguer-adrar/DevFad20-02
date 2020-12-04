af2ef810fc66326c82cd8ebf4cbb422657caf1da
Helixar <contact@cloudnode.fr>
2020-12-04 18:55:44 +0100

    feat : add crud + inner join on tables

---
+++ sql/crud/queries.sql
@@ -0,0 +1,13 @@
+use cours;
+
+select users.username, users.email from users where id = 2;
+
+update users set username = 'Helixarrrrr' where id = 3;
+
+delete from users where id = 4;
+
+select users.username, product_translations.name
+from carts
+         inner join users on carts.users_id = users.id
+         inner join products on carts.products_id = products.id
+         inner join product_translations on products.id = product_translations.id_products and product_translations.locale = 'en';
