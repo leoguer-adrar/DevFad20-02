a24081d00dbd4d94298ddd422c8e6178d1115782
Youffie-WX <72621528+Youffie-WX@users.noreply.github.com>
2020-12-04 18:56:24 +0100

    Update request MySQL
    Add Inner Join request

--- DATABASES/SQL/request.sql
+++ DATABASES/SQL/request.sql
@@ -20,7 +20,10 @@ VALUES ("Creations"),
 ("Recettes");
 
 INSERT INTO articles (titre_article, contenu_article, datecreation_article, datemodification_article, slug_article, id_categorie, id_utilisateur )
-VALUES ("Comment je créée mes bijoux", "Introduction.......", now(), now(), "blog/bijoux", 1, 1);
+VALUES ("Comment je créée mes bijoux", "Introduction.......", now(), now(), "blog/bijoux", 1, 1),
+("1ère recette de brownie", "Introduction.......", now(), now(), "blog/recette", 3, 1),
+("2ère recette de crêpes", "Introduction.......", now(), now(), "blog/recette", 3, 1),
+("Liste des produits", "paire de boucles d'oreilles", now(), now(), "blog/bijoux", 1, 1);
 
 -- modifier des données sur une table
 UPDATE categories
@@ -29,4 +32,13 @@ WHERE id=2;
 
 -- Supprimer des données sur une table
 DELETE FROM categories
-WHERE id=3;
+WHERE id=4;
+
+-- INNER JOIN explications
+-- Je cherche dans la table articles les attributs mentionnés dans le SELECT, 
+-- j'associe la table articles à la table categories 
+-- je lui dit que l'id 'id_categorie' de la table articles correspond à l'id 'id' de la table categories
+SELECT categories.nom_categorie, articles.titre_article, articles.contenu_article, articles.datecreation_article, articles.slug_article
+FROM articles
+INNER JOIN categories
+ON articles.id_categorie=categories.id
