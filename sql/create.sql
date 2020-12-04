1fec53747908b3b904ea0041f2c2504c44dab54c
Helixar <contact@cloudnode.fr>
2020-12-04 17:58:44 +0100

    fix : create and drop tables

--- sql/create.sql
+++ sql/create.sql
@@ -1,3 +1,17 @@
+use cours;
+
+SET FOREIGN_KEY_CHECKS = 0;
+drop table if exists users;
+drop table if exists carts;
+drop table if exists products;
+drop table if exists categories;
+drop table if exists carts_products;
+drop table if exists carts_products;
+drop table if exists products_categories;
+drop table if exists categories_translations;
+drop table if exists product_translations;
+SET FOREIGN_KEY_CHECKS = 1;
+
 #tables
 create table if not exists users
 (
@@ -15,6 +29,7 @@ create table if not exists categories
 (
     id          bigint unsigned auto_increment not null primary key,
     id_products bigint unsigned                not null,
+    code       varchar(255),
     active      tinyint unsigned default (1),
     created_at  datetime,
     updated_at  datetime
@@ -51,7 +66,6 @@ create table if not exists carts
     users_id    bigint unsigned                not null,
     products_id bigint unsigned                not null,
     count       Int,
-    id_users    Int,
     created_at  datetime,
     updated_at  datetime
 ) engine = InnoDB;
@@ -82,13 +96,13 @@ create table if not exists carts_products
 ) engine = InnoDB;
 
 alter table categories_translations
-    add constraint categories_translations_categories_FK foreign key (id_categories) REFERENCES categories (id);
+    add constraint categories_translations_categories_FK foreign key (id_categories) REFERENCES categories (id) on delete cascade;
 
 alter table product_translations
-    add constraint product_translations_products_FK foreign key (id_products) references products (id);
+    add constraint product_translations_products_FK foreign key (id_products) references products (id) on delete cascade;
 
 alter table carts
-    add constraint carts_users_FK foreign key (users_id) references users (id);
+    add constraint carts_users_FK foreign key (users_id) references users (id) on delete cascade;
 
 alter table products_categories
     add constraint products_categories_PK primary key (id_products, id_categories),
