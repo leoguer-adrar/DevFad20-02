53e451eae44d75d00d988bfcaa7db3ed39e529ca
MrMimi11 <remi.prat@laposte.net>
2021-01-07 13:49:32 +0100

    [FIX] all in English

--- SQL/projet.sql
+++ SQL/projet.sql
@@ -1,70 +1,71 @@
 use projet;
 
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE if EXISTS utilisateurs;
-DROP TABLE if EXISTS jeux;
-DROP TABLE if EXISTS bugs;
-DROP TABLE if EXISTS contacts;
-DROP TABLE if EXISTS commentaires;
+DROP TABLE if EXISTS User;
+DROP TABLE if EXISTS Game;
+DROP TABLE if EXISTS Bug;
+DROP TABLE if EXISTS Contact;
+DROP TABLE if EXISTS Comment;
 SET FOREIGN_KEY_CHECKS = 1;
 
-create table if not exists utilisateurs
+create table if not exists User
 (
-        id_utilisateur Int  Auto_increment PRIMARY KEY  NOT NULL ,
+        id_user       Int  Auto_increment PRIMARY KEY  NOT NULL ,
         pseudo        Varchar (255) NOT NULL ,
         email         Varchar (255) NOT NULL ,
-        motdepasse    Varchar (255) NOT NULL
+        password      Varchar (255) NOT NULL
 )ENGINE=InnoDB;
 
-CREATE TABLE if not exists jeux
+CREATE TABLE if not exists Game
 (
-        id_jeu  Int  Auto_increment  NOT NULL ,
+        id_game  Int  Auto_increment  NOT NULL ,
         id_bug  Int NOT NULL,
-        nom_jeu Varchar (255) NOT NULL,
-	    PRIMARY KEY (id_jeu)
+        name_game Varchar (255) NOT NULL,
+
+        PRIMARY KEY (id_game)
 )ENGINE=InnoDB;
 
-CREATE TABLE if not exists bugs
-(
+CREATE TABLE Bug(
+
         id_bug          Int  Auto_increment  NOT NULL ,
-        titre_bug       Varchar (255) NOT NULL ,
+        title_bug       Varchar (255) NOT NULL ,
         description_bug Varchar (255) NOT NULL ,
         video_bug       Varchar (255) NOT NULL ,
-        id_utilisateur  Int NOT NULL ,
-        id_jeu          Int NOT NULL,
-        datebug         DATETIME NOT NULL,
-	    PRIMARY KEY (id_bug)
+        date_bug        Datetime NOT NULL ,
+        id_user         Int ,
+        id_game         INT ,
+			PRIMARY KEY (id_bug)
+
 )ENGINE=InnoDB;
 
-CREATE TABLE if not exists contacts
-(
+
+CREATE TABLE Contact(
         id_contact Int  Auto_increment  NOT NULL ,
         pseudo     Varchar (255) NOT NULL ,
-        mail       Varchar (255) NOT NULL ,
-        message    Longtext NOT NULL,
-	    PRIMARY KEY (id_contact)
+        email      Varchar (255) NOT NULL ,
+        text       Longtext NOT NULL ,
+	PRIMARY KEY (id_contact)
 )ENGINE=InnoDB;
 
-CREATE TABLE if not exists commentaires
+CREATE TABLE if not exists Comment
 (
-        id_commentaire Int  Auto_increment  NOT NULL ,
-        id_utilisateur Int NOT NULL ,
-        reponse_a      Int NOT NULL ,
+        id_comment     Int  Auto_increment  NOT NULL ,
+        id_user        Int NOT NULL ,
+        answer         Int NOT NULL ,
         position       Int NOT NULL ,
         id_bug         Int NOT NULL ,
-	    PRIMARY KEY (id_commentaire)
+	    PRIMARY KEY (id_comment)
 )ENGINE=InnoDB;
 
-ALTER TABLE bugs
-    add constraint fk_bug_utilisateurs FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs(id_utilisateur);
-ALTER TABLE bugs
-	add constraint fk_bug_jeux FOREIGN KEY (id_jeu) REFERENCES jeux(id_jeu);
+ALTER TABLE Bug
+    add constraint fk_bug_user FOREIGN KEY (id_user) REFERENCES user(id_user);
+ALTER TABLE Bug
+	add constraint fk_bug_game FOREIGN KEY (id_game) REFERENCES game(id_game);
 
-ALTER TABLE commentaires
-    add constraint fk_commentaire_bugs FOREIGN KEY (id_bug) REFERENCES bugs(id_bug);
-ALTER TABLE commentaires
-	add constraint fk_commentaire_utilisateurs FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs(id_utilisateur);
-
+ALTER TABLE Comment
+    add constraint fk_comment_bug FOREIGN KEY (id_bug) REFERENCES bug(id_bug);
+ALTER TABLE Comment
+	add constraint fk_comment_user FOREIGN KEY (id_user) REFERENCES user(id_user);
 
 -- SELECT jeux.nom_jeu,bugs.titre_bug, bugs.description_bug, bugs.video_bug -- Je cherche dans la table bugs les attributs du SELECT, 
 -- FROM bugs --de bugs
--- SQL/requete_projet.sql
+++ SQL/requete_projet.sql
@@ -1,41 +1,41 @@
 use projet;
 
 SET FOREIGN_KEY_CHECKS = 0;
-TRUNCATE  utilisateurs;
-TRUNCATE  jeux;
-TRUNCATE  bugs;
-TRUNCATE  contacts;
-TRUNCATE  commentaires;
+TRUNCATE  user;
+TRUNCATE  game;
+TRUNCATE  bug;
+TRUNCATE  contact;
+TRUNCATE  comment;
 SET FOREIGN_KEY_CHECKS = 1;
 
-INSERT INTO jeux(id_bug, nom_jeu) 
+INSERT INTO game(id_bug, name_game) 
 Values (1, "Zelda Ocarina of time"),
 (2, "Zelda Majora's Mask");
 
-INSERT INTO utilisateurs(pseudo, email, motdepasse)
+INSERT INTO user(pseudo, email, password)
 Values ("AB", "CD", "EF"),
 ("AB", "CD", "EF"),
 ("AB", "CD", "EF");
 
-INSERT INTO bugs(titre_bug, description_bug, video_bug, id_utilisateur, id_jeu, datebug) 
-Values ("Wrong Wrap", "teleportation...", "htpp...", 2, 1, now());
+INSERT INTO bug(title_bug, description_bug, video_bug, id_user, id_game, date_bug) 
+Values ("Wrong Warp", "teleportation...", "htpp...", 2, 1, now());
 
-UPDATE jeux 
-set nom_jeu ="Zelda ocarina of Time 64"
-WHERE id_jeu=1;
+UPDATE game 
+set name_game ="Zelda ocarina of Time 64"
+WHERE id_game=1;
 
-UPDATE jeux 
-set nom_jeu ="Zelda Majora's Mask 64"
-WHERE id_jeu=2;
+UPDATE game 
+set name_game ="Zelda Majora's Mask 64"
+WHERE id_game=2;
 
-UPDATE utilisateurs
+UPDATE user
 SET pseudo="Mimi"
-WHERE id_utilisateur=1;
+WHERE id_user=1;
 
-DELETE FROM utilisateurs
-WHERE id_utilisateur=3;
+DELETE FROM user
+WHERE id_user=3;
 
-SELECT jeux.nom_jeu,bugs.titre_bug, bugs.description_bug, bugs.video_bug -- Je cherche dans la table bugs les attributs du SELECT, 
-FROM bugs --de bugs
-inner JOIN jeux --dans table jeux
-ON bugs.id_jeu = jeux.id_jeu	-- je lui dis que l'id id_jeu de la table bugs correspond à l'id id_jeu de la table jeux
+SELECT game.name_game,bug.title_bug, bug.description_bug, bug.video_bug -- Je cherche dans la table bugs les attributs du SELECT, 
+FROM bug --de bugs
+inner JOIN game --dans table jeux
+ON bug.id_game = game.id_game	-- je lui dis que l'id id_jeu de la table bugs correspond à l'id id_jeu de la table jeux
