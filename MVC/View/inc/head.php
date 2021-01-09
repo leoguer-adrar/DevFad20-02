0c2ee3eb6cf615a495312646b822116beb708562
Youffie-WX <72621528+Youffie-WX@users.noreply.github.com>
2021-01-09 17:50:41 +0100

    Create Inc Files HTML
    Ajout des fichiers en-têtes et footer page HTML

---
+++ MVC/View/inc/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Styles sheets -->
+<link rel="stylesheet" href="../CSS/styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
+       <title>Blog Ju-Petillant</title>
+</head>
---
+++ MVC/View/inc/header.php
@@ -0,0 +1,53 @@
+<body class="bg-light">
+
+    <nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
+        <button class="navbar-toggler sideMenuToggler" type="button">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="http://localhost/TP_PHP/projet%20ju-petillant/Public/index.php#"> Ju-Petillant</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav auto ml-3">
+                <li class="nav-item dropdown">
+
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Articles
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="http://localhost/TP_PHP/projet%20ju-petillant/Controller/Article.controller.php#">Afficher les articles</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="http://localhost/TP_PHP/projet%20ju-petillant/View/createArticle.php">Ajouter un article</a>
+                        <a class="dropdown-item" href="#">Modifier un article</a>
+                        <a class="dropdown-item" href="#">Supprimer un article</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Catégories
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Afficher les catégories</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Ajouter une catégorie</a>
+                        <a class="dropdown-item" href="#">Modifier une catégorie</a>
+                        <a class="dropdown-item" href="#">Supprimer une catégorie</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Utilisateurs
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="http://localhost/TP_PHP/projet%20ju-petillant/Controller/User.controller.php#">Afficher les utilisateurs</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Ajouter un utilisateur</a>
+                        <a class="dropdown-item" href="#">Modifier un utilisateur</a>
+                        <a class="dropdown-item" href="#">Supprimer un utilisateur</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
